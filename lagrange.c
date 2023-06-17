a808a62e327838aaee572bf853dd2b3b60b9d812
Julian Sasso <jsasso@itba.edu.ar>
2023-06-17 11:45:50 -0300

    Minor fixes in lagrange.c

--- lagrange.c
+++ lagrange.c
@@ -1,12 +1,12 @@
 #include <stdarg.h>
 
-#define K = 4
+#define K 4
 
 /*
 falta definir xp = punto de interpolacion
 */
 void lagrange(int sh1[], int sh2[], int sh3[], int sh4[]) {
-    int x[K], y[K];
+    int x[K], y[K], p, yp, xp = 0;
 
     x[0] = sh1[0];
     x[1] = sh2[0];
@@ -18,9 +18,9 @@ void lagrange(int sh1[], int sh2[], int sh3[], int sh4[]) {
     y[2] = sh3[1];
     y[3] = sh4[1];
 
-    for(i = 1; i <= K;i++) {
-        p=1
-		for(j = 1;j <= K;j++) {
+    for(int i = 1; i <= K;i++) {
+        p = 1;
+		for(int j = 1;j <= K;j++) {
 			if(i != j) {
 			    p *= (xp - x[j])/(x[i] - x[j]);
 			}
