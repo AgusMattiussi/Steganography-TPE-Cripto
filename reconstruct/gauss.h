c9055fe508704d01c329befdf1e847ffc8303f0b
Julian Sasso <jsasso@itba.edu.ar>
2023-06-19 18:37:26 -0300

    Implement r_i comparison logic

---
+++ reconstruct/gauss.h
@@ -0,0 +1,15 @@
+#ifndef _GAUSS_H
+#define _GAUSS_H
+
+#include <stdint.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "../utils/utils.h"
+
+#define GROUP_MOD 251
+
+uint8_t * gauss(uint8_t * y, uint8_t * x, int dim);
+void triangulate(uint8_t ** m, int dim);
+
+#endif
