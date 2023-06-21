bacc3cb7096b281488ce82a1d969abdfc5528b59
Julian Sasso <jsasso@itba.edu.ar>
2023-06-21 01:06:44 -0300

    Refactor and comment gauss.c

--- src/gauss.c
+++ src/gauss.c
@@ -1,9 +1,10 @@
 #include "include/gauss.h"
 
 static uint8_t * solve(uint8_t ** m, int dim);
+static void triangulate(uint8_t ** m, int dim);
 
 uint8_t * gauss(uint8_t * y, uint8_t * x, int dim){
-    /* Reserva espacio para la matriz extendida*/
+    /* Allocates space for the extended matrix */
     uint8_t ** gaussMatrix = allocateMatrix(dim, dim + 1);
 
     for (int i = 0; i < dim; i++){
@@ -17,22 +18,21 @@ uint8_t * gauss(uint8_t * y, uint8_t * x, int dim){
     uint8_t * solutions = solve(gaussMatrix, dim);
 
     freeMatrix(gaussMatrix, dim);
-    
     return solutions;
 }
 
-void triangulate(uint8_t ** m, int dim){
+/* Applies elimination to create an upper triangular matrix */
+static void triangulate(uint8_t ** m, int dim){
     for (int i = 0; i < dim - 1; i++){
         uint8_t inverse = MOD_INVERSE(m[i][i]);
 
-        /* Multiplica esta fila por su inverso multiplicativo para
-            que el primer coeficiente sea 1 */
+        /* Multiplies current row by the modular inverse of its first coefficient,
+           so it becomes 1 */
         for (size_t z = i; z < dim + 1; z++){
             m[i][z] = (m[i][z] * inverse) % GROUP_MOD;
         }
 
-        /* A cada fila, le resta un multiplo de la fila actual para anular
-            su primer coeficiente */
+        /* Substracts a multiple of the correspondig upper row to each lower row */
         for (int j = i + 1; j < dim; j++){
             int ratio = m[j][i];
             for (int k = i; k < dim + 1; k++){
@@ -42,19 +42,20 @@ void triangulate(uint8_t ** m, int dim){
     }
 }
 
+/* Solves an upper triangular matrix equation system */
 static uint8_t * solve(uint8_t ** m, int dim){
     uint8_t * solutions = calloc(dim, sizeof(int));
     
     for (int i = dim-1; i >= 0; i--){
-        /* Resultado de esta fila*/
+        /* y value for this row */
         int accum = m[i][dim];
 
-        /* Usa las incognitas encontradas para despejar la ecuacion */
+        /* Use known solutions to partially solve current equation*/
         for (int j = dim-1; j > i; j--){
             accum -= m[i][j] * solutions[j];
         }
 
-        /* Despeja una de las incognitas */
+        /* Finds current coeficcient */
         solutions[i] = (positiveMod(accum) * MOD_INVERSE(m[i][i])) % GROUP_MOD;
     }
 
--- src/include/gauss.h
+++ src/include/gauss.h
@@ -4,11 +4,9 @@
 #include <stdint.h>
 #include <math.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include "utils.h"
 
+/* Solves a system of 'dim' equations using Gauss' elimination method */
 uint8_t * gauss(uint8_t * y, uint8_t * x, int dim);
-
-void triangulate(uint8_t ** m, int dim);
 
 #endif
